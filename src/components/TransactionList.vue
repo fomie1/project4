e87d64c6c4f16384ee5120edf901b6d4ec9fdf2f
Daniel Garza <dgarz160@stu.southtexascollege.edu>
2024-09-22 15:57:45 -0500

    further changes for steam wishlist

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -20,14 +20,15 @@
 
 
 <template>
-    <h3>History</h3>
+    <h3>Games On Wishlist:</h3>
+    <h5>Click the X to remove a wishlisted game</h5>
     <ul id="list" class="list">
         <li v-for="x in transactions" v-bind:key="x.id"
         v-bind:class="x.amount < 0 ? 'minus' : 'plus'">
         {{ x.text }} <span> ${{ x.amount }}</span>
         <button @click="deleteTransaction(x.id)" class="delete-btn">X</button>
     </li>
-
+    
     </ul>
 
 
